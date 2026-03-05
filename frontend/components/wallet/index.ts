669bd4964c2b887e010ccc1c56a221b8528341c6
Vo Quang Chien <2giosangmitom@proton.me>
2026-03-05 15:55:06 +0700

    clean up

--- frontend/components/wallet/index.ts
+++
@@ -1,4 +0,0 @@
-export { WalletProvider } from './WalletProvider';
-export { WalletButton } from './WalletButton';
-export { WalletConnect } from './WalletConnect';
-export { WalletInfo } from './WalletInfo';
