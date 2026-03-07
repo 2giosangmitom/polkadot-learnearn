75882bbfe501336fb49501605eefa251a604a21f
Vo Quang Chien <2giosangmitom@proton.me>
2026-03-07 20:11:01 +0700

    allow all origins

--- api/main.py
+++ api/main.py
@@ -50,9 +50,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        origin.strip() for origin in settings.CORS_ORIGINS.split(",") if origin.strip()
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
